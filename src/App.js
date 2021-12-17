152a2921d0e445aae632bb9528cece06fae940e6
ivaninastoyanova <72800115+ivaninastoyanova@users.noreply.github.com>
2021-12-17 19:05:59 +0200

    Set protected routes.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import { Route, Routes } from "react-router";
-import "./App.css";
+import useLocalStorage from "./hooks/useLocalStorage";
+import { useEffect } from "react";
 
 import { AuthProvider } from "./contexts/AuthContext";
 import { NotificationProvider } from "./contexts/NotificationContext";
-
-import Notification from "./components/Common/Notification";
-import ErrorBoundary from "./components/Common/ErrorBoundary";
-import PrivateRoute from "./components/Common/PrivateRoute";
-import GuardedRoute from "./components/Common/GuardedRoute";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import Logout from './components/Logout';
-
-import AuthContext from "./contexts/AuthContext";
-import useLocalStorage from "./hooks/useLocalStorage";
-
-import { useEffect } from "react";
-
-import RecipesList from "./components/Recipes/RecipesList";
+import Logout from "./components/Logout";
 import MyRecipes from "./components/MyRecipes/MyRecipes";
 import AboutUs from "./components/AboutUs";
 import Create from "./components/Create";
 import Details from "./components/Details";
 import Catalog from "./components/Catalog";
-import Edit from './components/Edit';
+import Edit from "./components/Edit";
+
+import Notification from "./components/Common/Notification";
+import ErrorBoundary from "./components/Common/ErrorBoundary";
+import ProtectedRoute from "./components/Common/ProtectedRoute";
+
+import "./App.css";
+
 
 function App() {
   const [user, setUser] = useLocalStorage("user", null);
@@ -52,15 +48,13 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/logout" element={<Logout />} />
                 <Route path="/catalog" element={<Catalog />} />
-                <Route path="/my-recipes" element={<MyRecipes />} />
                 <Route path="/about" element={<AboutUs />} />
-                <Route path="/create" element={<Create />} />
                 <Route path="/details/:recipeId" element={<Details />} />
-                <Route path="/edit/:recipeId" element={<Edit />} />
 
-
-                <Route element={<GuardedRoute />}>
-
+                <Route element={<ProtectedRoute />}>
+                  <Route path="/my-recipes" element={<MyRecipes />} />
+                  <Route path="/create" element={<Create />} />
+                  <Route path="/edit/:recipeId" element={<Edit />} />
                 </Route>
 
                 <Route
@@ -73,7 +67,6 @@ function App() {
             </main>
 
             <Footer />
-
           </div>
         </NotificationProvider>
       </AuthProvider>
--- src/components/Common/ProtectedRoute.js
+++ src/components/Common/ProtectedRoute.js
@@ -1,10 +1,10 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthContext } from '../../contexts/AuthContext';
 
-const GuardedRoute = () => {
+const ProtectedRoute = () => {
     const { isAuthenticated } = useAuthContext();
 
     return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
 }
 
-export default GuardedRoute;
+export default ProtectedRoute;
