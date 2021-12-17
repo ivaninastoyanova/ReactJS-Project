f49950ee42dc8df0d32602e22cd9c8bcfd8ca518
ivaninastoyanova <72800115+ivaninastoyanova@users.noreply.github.com>
2021-12-17 13:04:30 +0200

    Made the details component work with the server. Add some css.

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,39 +1,116 @@
-import { Link } from "react-router-dom";
 import "./Details.css";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
 
-export default function Details() {
+import * as recipeService from "../../services/recipeService";
+import * as likeService from "../../services/likeService";
+import { useAuthContext } from "../../contexts/AuthContext";
+import {
+  useNotificationContext,
+  types,
+} from "../../contexts/NotificationContext";
+import useRecipeState from "../../hooks/useRecipeState";
 
+import ConfirmDialog from "../Common/ConfirmDialog";
+
+const Details = () => {
+  const navigate = useNavigate();
+  const { user } = useAuthContext();
+  const { addNotification } = useNotificationContext();
+  const { recipeId } = useParams();
+  const [recipe, setRecipe] = useRecipeState(recipeId);
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+  useEffect(() => {
+    likeService.getRecipeLikes(recipeId).then((likes) => {
+      setRecipe((state) => ({ ...state, likes }));
+    });
+  }, []);
+
+  const deleteHandler = (e) => {
+    e.preventDefault();
+
+    recipeService
+      .destroy(recipeId, user.accessToken)
+      .then(() => {
+        navigate("/catalog");
+      })
+      .finally(() => {
+        setShowDeleteDialog(false);
+      });
+  };
+
+  const deleteClickHandler = (e) => {
+    e.preventDefault();
+    console.log(process.env.NODE_ENV);
+    setShowDeleteDialog(true);
+  };
+
+  const likeButtonClick = () => {
+    if (user._id === recipe._ownerId) {
+      return;
+    }
+
+    if (recipe.likes.includes(user._id)) {
+      addNotification("You cannot like a recipe more than once.");
+      return;
+    }
+
+    likeService.like(user._id, recipeId).then(() => {
+      setRecipe((state) => ({ ...state, likes: [...state.likes, user._id] }));
+
+      addNotification("Successfuly liked a recipe.", types.success);
+    });
+  };
+
+  const ownerButtons = (
+    <>
+      <Link className="button" to={`/edit/${recipe._id}`}>
+        <button className="details-owner-button"> Edit </button>
+      </Link>
+      <button className="details-owner-button" onClick={deleteClickHandler}> Delete </button>
+    </>
+  );
+
+  const userButtons = (
+      <button className="details-guest-button" onClick={likeButtonClick} disabled={recipe.likes?.includes(user._id)}>Like</button>
+  );
 
   return (
     <>
+    <ConfirmDialog show={showDeleteDialog} onClose={() => setShowDeleteDialog(false)} onSave={deleteHandler} />
       <section id="details">
-
         <article className="details-top">
           <section className="details-top-left">
-            <h2 className="details-title">Recipe Title</h2>
-            <h3 className="details-type">Recipe Type: </h3>
-            <p className="details-likes-count">Likes: 0 </p>
+            <h2 className="details-title">{recipe.name}</h2>
+            <h3 className="details-type">Type: {recipe.type} </h3>
+            <p className="details-likes-count">Likes:  {recipe.likes?.length || 0} </p>
 
             <article className="details-buttons">
-              <button className="details-owner-button"> Edit </button>
-              <button className="details-owner-button"> Delete </button>
-              <button className="details-guest-button"> Like </button>
+              {user._id && (user._id == recipe._ownerId 
+                ? ownerButtons 
+                : userButtons)}
             </article>
           </section>
 
           <section className="details-top-right">
-            <img className="details-top-right-img" src="./images/food3.jpg" alt="recipe" />
+            <img
+              className="details-top-right-img"
+              src={recipe.imageUrl}
+              alt="recipe"
+            />
           </section>
         </article>
 
         <article className="details-bottom">
           <h2 className="details-description-title">Description:</h2>
           <p className="details-description-text">
-            Lorem ipsum dolor sit amet consectetur loren Lorem ipsum Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius vero nemo rerum, magnam voluptate cupiditate exercitationem, culpa ipsa minus deleniti possimus? Distinctio ad ut repellendus! Deserunt ea ipsum hic. dolor sit amet consectetur adipisicing elit. Laboriosam, quam sed fuga fugiat assumenda saepe hic voluptatum nulla accusamus, harum dolore vitae nihil eius dolorum facilis modi asperiores soluta architecto. adipisicing. Quisqus doloribus tempora earum! Facilis porro eos necessitatibus accusamus beatae obcaecati culpa!
+            {recipe.description}
           </p>
         </article>
-
       </section>
     </>
   );
-}
+};
+
+export default Details;
--- src/services/likeService.js
+++ src/services/likeService.js
@@ -4,7 +4,7 @@ const baseUrl = "http://localhost:3030/data";
 
 export const like = (userId, recipeId) =>
   request.post(`${baseUrl}/likes`, { userId, recipeId });
-export const getPetLikes = (recipeId) => {
+export const getRecipeLikes = (recipeId) => {
   const query = encodeURIComponent(`recipeId="${recipeId}"`);
 
   return request
